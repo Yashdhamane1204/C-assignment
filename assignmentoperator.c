93c059e7651a30199d32c104ffbd9cd54c2b3628
Yashdhamane1204 <ynd0808@gmail.com>
2024-10-29 21:30:44 +0530

    Add files via upload

---
+++ assignmentoperator.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+
+int main() {
+     int a,b;
+     a=13;
+     b=17;
+     //operationforadditionassignment//
+     printf("a+=b=%d\n",a+=b);
+     //operationforsubtractionassignment//
+     printf("a-=b=%d\n",a-=b);
+     //operationfordivisionassignment//
+     printf("a/=b=%d\n",a<=b);
+     //operationformultiplicationassignment//
+     printf("a*=b=%d\n",a*=b);
+     //operationformodulusassignment//
+     printf("a%=b=%d\n",a%=b);
+     //operationforassignment//
+     printf("a=b=%d\n",a=b);
+     return 0;
+     }
+     
+     
+    
