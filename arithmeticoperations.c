e66ea8f79d448880e4220273c3af3038272308ad
Yashdhamane1204 <ynd0808@gmail.com>
2024-10-29 21:17:43 +0530

    Add files via upload

---
+++ arithmeticoperations.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+
+int main() {
+     int a,b;
+     a=13;
+     b=17;
+     //operationforaddition//
+     printf("a+b=%d\n",a+b);
+     //operationforsubtraction//
+     printf("a-b=%d\n",a-b);
+     //operationformultiplication//
+     printf("a*b=%d\n",a*b);
+     //operationfordivision//
+     printf("a/b=%d\n",a/b);
+     //operationformodulus//
+     printf("a%b=%d\n",a%b);
+     return 0;
+     }
+     
+     
+    
