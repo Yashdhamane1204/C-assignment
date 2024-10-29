b43dfcfceadeab97929a679814497bc99c622949
Yashdhamane1204 <ynd0808@gmail.com>
2024-10-29 21:21:06 +0530

    Add files via upload

---
+++ relationaloperater.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+
+int main() {
+     int a,b;
+     a=13;
+     b=17;
+     //operationforlesser//
+     printf("a<b=%d\n",a<b);
+     //operationforgreater//
+     printf("a>b=%d\n",a>b);
+     //operationforlesserthan//
+     printf("a<=b=%d\n",a<=b);
+     //operationforgreaterthan//
+     printf("a>=b=%d\n",a>=b);
+     //operationforequalto//
+     printf("a=b=%d\n",a=b);
+     //operationfordoubleequalto//
+     printf("a==b=%d\n",a==b);
+     //operationfornotequalto//
+     printf("a!=b=%d\n",a!=b);
+     return 0;
+     }
+     
+     
+    
