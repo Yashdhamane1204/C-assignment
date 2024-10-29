c764b29ed0299d245bedad1f64dd7177bdab13c2
Yashdhamane1204 <ynd0808@gmail.com>
2024-10-29 21:20:05 +0530

    Add files via upload

---
+++ logicaloperators.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+
+int main() {
+     int a,b;
+     a=13;
+     b=17;
+     //operationforAND//
+     printf("a&&b=%d\n",a&&b);
+     //operationforOR//
+     printf("a||b=%d\n",a||b);
+     //operationforNOR//
+     printf("a!=b=%d\n",a!=b);
+     return 0;
+     }
+     
+     
+    
